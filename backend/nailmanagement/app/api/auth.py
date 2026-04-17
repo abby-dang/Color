e89635f6a085d0919060987412ed269254abc9e8
abby-dang <abby.dang62@gmail.com>
2026-04-16 20:11:33 -0600

    Authentication mvp finished

--- backend/nailmanagement/app/api/auth.py
+++ backend/nailmanagement/app/api/auth.py
@@ -1,16 +1,26 @@
 import json
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 from nailmanagement.app.services.auth import UserAuthentication
 
 auth = UserAuthentication()
 
 #Add checks for duplicate emails/usernames
+@csrf_exempt
 def register(request):
     if request.method == "POST":
-        body = json.loads(request.body)
-        email = body["email"]
-        password = body["password"]
+        try:
+            body = json.loads(request.body)
+            email = body["email"]
+            password = body["password"]
 
-        response = auth.sign_up(email, password)
-        return JsonResponse({"user": response.user, "token": response.session.access_token})
+            response = auth.sign_up(email, password)
+            return JsonResponse({
+                "id": str(response.user.id),
+                "email": response.user.email,
+                "token": response.session.access_token if response.session else None
+            })
+        
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status = 400)
     
--- backend/nailmanagement/app/db/supabase_client.py
+++ backend/nailmanagement/app/db/supabase_client.py
@@ -1,6 +1,13 @@
 import os
+from pathlib import Path
 from supabase import create_client, Client
+from dotenv import load_dotenv
 
-url: str = os.environ.get("https://ytueocpenouaflaqlrlv.supabase.co")
-key: str = os.environ.get("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl0dWVvY3Blbm91YWZsYXFscmx2Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3NTcwODA3NiwiZXhwIjoyMDkxMjg0MDc2fQ.5NGYsXz773dSEeWb80WfOun9iXlzEMnXsF58c0n0zEA")
+load_dotenv(Path(__file__).resolve().parent.parent.parent.parent / '.env')
+
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
+
+print(f"URL: {url}") 
+
 supabase: Client = create_client(url, key)
